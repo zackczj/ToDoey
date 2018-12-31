60985c075905289f5c65ce600ba9308a8c892d37
zack <chngzzj@gmail.com>
2018-12-31 21:50:37 +0800

    Initial Table View Setup

---
+++ ToDoey/ToDoListVC.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Zack Chng on 31/12/18.
+//  Copyright © 2018 Zack. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListVC: UITableViewController {
+
+    let itemArray = ["Find Mike", "Gorgon", "Placeholder"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+}
+
